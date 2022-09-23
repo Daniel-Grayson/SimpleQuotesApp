4a0a531ee0ba062bb8061661ca0fe25e243ffc19
Daniel Grayson <graysoncodes@gmailcom>
2022-09-23 21:10:31 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 import 'UI/home.dart';
 
 void main(List<String> args) => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Wisdom(),
     ));
